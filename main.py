59e3f285a8e944b49e456a872a1fadfd981b1394
Gui0delp <delpro.guillaume@gmail.com>
2019-04-14 16:54:30 +0200

    Add a conditional structure for know if player win or... not

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ def main():
     """The function contents the main code of the game
     """
     game = True
+    quit_game = True
     w_sprite = 40
     l_sprite = 40
     w_width = 15 * w_sprite
     w_length = 15 * l_sprite
+    game_statut = 2    #0 = Loose, 1 = Win, 2 = Nothing
     pygame.init() # pylint: disable=no-member
     pygame.display.set_caption("Macgyver maze")
     window = pygame.display.set_mode((w_width, w_length))
@@ -23,9 +25,22 @@ def main():
     dir_file = "level_1.txt"
     main_lvl = lvl.GameLvl(dir_file)
     main_lvl.generate_lvl()
+    #Use the structure generate by the methode generate_lvl() from the class GameLvl()
     macgyver = mac.Player(main_lvl.game_level)
-
-    while game:    #The main loop of the game
+    #The main loop of the game
+    while game:
+        #Permit to know if the player win or... not
+        if macgyver.lvl[macgyver.y_mac][macgyver.x_mac] == "G" and macgyver.u_obj == 3:
+            macgyver.y_mac = 14
+            macgyver.x_mac = 0
+            main_lvl.game_level[13][0] = "S"
+            game_statut = 1
+            game = False
+        elif macgyver.lvl[macgyver.y_mac][macgyver.x_mac] == "G" and macgyver.u_obj != 3:
+            macgyver.y_mac = 14
+            macgyver.x_mac = 2
+            game_statut = 0
+            game = False
 
         main_lvl.refresh_lvl(window)
         macgyver.move(window, "")
@@ -42,11 +57,19 @@ def main():
                     macgyver.move(window, "RIGHT")
                 elif event.key == pygame.K_ESCAPE:    # pylint: disable=no-member
                     game = False
-
+        #Call the methode user_interface for manage the objects counter and more
+        macgyver.user_interface(window, game_statut)
         pygame.display.flip()
 
-    pygame.quit() # pylint: disable=no-member
+    while quit_game:
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:    # pylint: disable=no-member
+                if event.key == pygame.K_ESCAPE:    # pylint: disable=no-member
+                    quit_game = False
+                    pygame.quit() # pylint: disable=no-member
 
 
 if __name__ == "__main__":
+    #Call the main() function of the game
     main()
